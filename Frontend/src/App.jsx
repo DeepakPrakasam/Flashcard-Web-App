a04555294628f60818dc9c8c4e2a66b4f9742d28
DeepakPrakasam <deepakp.22cse@kongu.edu>
2025-02-14 23:00:35 +0530

    Added create and delete feature

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState,useEffect } from "react";
+import {nanoid} from 'nanoid';
+import Notes from "./components/Notes";
+import NotesList from "./components/NotesList";
+import Search from "./components/Search";
+import Header from "./components/Header";
 
-function App() {
+const App= ()=>{
+  const [notes, setNotes]= useState([
+    {
+    id: nanoid(),
+    text:"This is my first note!!!",
+    date:"14/02/2025"},
+    {
+      id: nanoid(),
+      text:"This is my second note!!!",
+      date:"12/02/2025"},
+      {
+        id: nanoid(),
+        text:"This is my third note!!!",
+        date:"13/02/2025"},
+        {
+          id: nanoid(),
+          text:"This is my new note!!!",
+          date:"13/02/2025"},
+  ]);
 
-  return (
-    <>
-      <h2>Welcome To MindFlare⚡</h2>
-    </>
+  const addNote = (text) => {
+		const date = new Date();
+		const newNote = {
+			id: nanoid(),
+			text: text,
+			date: date.toLocaleDateString(),
+		};
+		const newNotes = [...notes, newNote];
+		setNotes(newNotes);
+	};
+
+  const deleteNote = (id) => {
+		const newNotes = notes.filter((note) => note.id !== id);
+		setNotes(newNotes);
+	};
+
+  const [searchText, setSearchText] = useState('');
+
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+		const savedNotes = JSON.parse(
+			localStorage.getItem('react-notes-app-data')
+		);
+
+		if (savedNotes) {
+			setNotes(savedNotes);
+		}
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem(
+			'react-notes-app-data',
+			JSON.stringify(notes)
+		);
+	}, [notes]);
+
+  return(
+    <div className={`${darkMode && 'dark-mode'}`}>
+			<div className='container'>
+				<Header handleToggleDarkMode={setDarkMode} />
+				<Search handleSearchNote={setSearchText} />
+				<NotesList
+					notes={notes.filter((note) =>
+						note.text.toLowerCase().includes(searchText)
+					)}
+					handleAddNote={addNote}
+					handleDeleteNote={deleteNote}
+				/>
+			</div>
+		</div>
   )
-}
+};
 
-export default App
+export default App;
